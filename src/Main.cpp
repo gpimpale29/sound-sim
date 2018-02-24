43faa26d14a045e5db390c2daf9bf29b5cedc7a8
fidgetsinner <gpimpale29@gmail.com>
2018-02-23 22:05:48 -0800

    First Commit lets go

---
+++ src/Main.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : RoughSound.cpp
+// Author      : Govind
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "thing" << endl; // prints thing
+	return 0;
+}
